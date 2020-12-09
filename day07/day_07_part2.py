311497c0bcba21726dcb7c7bbba0635347617348
Petra <disi77gmail.com>
2020-12-09 20:18:24 +0100

    Day 7 fix

--- day07/day_07_part2.py
+++ day07/day_07_part2.py
@@ -27,46 +27,6 @@ def get_all_rules(data):
     return rules
 
 
-def get_rules_for_santa(rules, santa_bag):
-    """
-    Find list of rules connected with Santa's bag in format:
-    (color1, num, color2)
-    '<color1> bags contain <num> <color2> bags'
-    """
-    rules_santa = []
-
-    colors = []
-    temp_colors = []
-
-    # Start with rules where is "shiny gold" bag on the left side
-    for rule in rules:
-        if rule[0] == santa_bag:
-            temp_colors.append(rule[2])
-            rules_santa.append(rule)
-
-    # Then for each of the rule with "shiny gold" bag fint the children rule
-    while True:
-        temp = []
-        for color in temp_colors:
-            for rule in rules:
-                if color == rule[0]:
-                    temp.append(rule[2])
-                    rules_santa.append(rule)
-
-        # All used colors saved in the list "colors"
-        colors.extend(temp_colors)
-        colors = list(set(colors))
-
-        # Set colors for processing in next round
-        temp_colors = list(temp)
-
-        if not temp_colors:
-            break
-
-    # Return list of Santa's rules
-    return list(set(rules_santa))
-
-
 def get_count_of_bags(pointer):
     """
     Counter of bags
@@ -84,9 +44,8 @@ def get_count_of_bags(pointer):
     return result
 
 
-rules_all = get_all_rules(input_data)
+rules_santa = get_all_rules(input_data)
 santa_bag = "shiny gold"
-rules_santa = get_rules_for_santa(rules_all, santa_bag)
 result = get_count_of_bags(santa_bag)
 
 print("How many individual bags are required inside single shiny gold bag?")
