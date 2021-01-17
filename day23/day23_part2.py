aafe1b402e08976c2cade3c044ead811b67bcf71
Petra <disi77gmail.com>
2021-01-17 23:07:05 +0100

    Day 23 part 2

--- day23/day23_part2.py
+++ day23/day23_part2.py
@@ -3,6 +3,11 @@ input_data = "467528193"
 
 class Cups_game:
     def __init__(self, input_data):
+        """
+        Create start settings for game
+        """
+        # Create "linked list" as dictionary for input data + values from
+        # 10 to 1,000,000 inclusive
         self.cups = {}
         data = [int(x) for x in input_data] + list(range(10, 1_000_001))
         for index, item in enumerate(data):
@@ -11,11 +16,19 @@ class Cups_game:
             else:
                 self.cups[item] = data[0]
         self.max = max(self.cups)
+        # Set first "current cup", first "3 picked up cups" and first "destination cup"
         self.current = int(input_data[0])
         self.picked_up_cups = [int(input_data[1]), int(input_data[2]), int(input_data[3])]
         self.destination = self.calculate_destination()
 
     def calculate_destination(self):
+        """
+        Calculate destination cup.
+        Destination cup is -1 from current cup.
+        Destination is not between picked up cups.
+        If destination cup is potentionaly == 0, then use the biggest
+        possible cup.
+        """
         destination = self.current - 1
         while destination in self.picked_up_cups:
             destination -= 1
@@ -26,6 +39,11 @@ class Cups_game:
         return destination
 
     def move(self):
+        """
+        Move in game.
+        Paste picked up cups after destination cup.
+        Calculate new current cup, new picked up cups and new destination cup.
+        """
         self.cups[self.current] = self.cups[self.picked_up_cups[2]]
         self.cups[self.picked_up_cups[2]] = self.cups[self.destination]
         self.cups[self.destination] = self.picked_up_cups[0]
@@ -37,6 +55,8 @@ class Cups_game:
         self.destination = self.calculate_destination()
 
 
+# Do 10,000,000 moves and find out the first and second cup after cup 1
+# As result return multiplication of these two cups
 a = Cups_game(input_data)
 for i in range(10_000_000):
     a.move()
